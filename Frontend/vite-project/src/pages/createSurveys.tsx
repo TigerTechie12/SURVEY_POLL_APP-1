b34605f9014e0dd39648b2e90ef7fecaa9359761
Shreyash <shreyashiitr22@gmail.com>
2025-06-01 22:46:24 +0530

    updated frontend

--- Frontend/vite-project/src/pages/createSurveys.tsx
+++ Frontend/vite-project/src/pages/createSurveys.tsx
@@ -1,21 +1,35 @@
 import {BACKEND_URL} from "../config/index"
 import{useState,useEffect} from "react"
 import axios from 'axios'
+type Option=string
+interface Question{
+    questionText:string,
+    options:Option[]
+}
 export function CreateSurveys(){
-    return <div className="flex flex-col">
-        <div className="font-bold">Create New Survey</div>
-        <div>Design your survey with multiple questions & options</div>
-        <div className="font-semibold">Survey Title *</div>
-        <input type='text' placeholder="e.g.,Customer Satisfaction Survey" className="rounded"></input>
-       <div className="font-semibold">Description *</div>
-       <input type="text" className="rounded" placeholder="Briefly describe what this survey is about and why you are conducting it"  />
-<div className="flex flex-row justify-between ">
-    <div className="font-bold">Survey Questions</div>
-    <button className="flex items-center gap-2 bg-green text-white px-4 py-2 rounded-2xl hover:bg-green-800">
-   <span className="text-lg font-bold">＋</span>
-      <span className="font-semibold">Add Question</span>
-    </button>
-</div>
+const [questions,setQuestions]=useState<Question[]>([{
+    questionText:'',options:["",""]
+}])
+const [title,setTitle]=useState('')
+const [description,setDescription]=useState('')
+function AddQuestions(){
+    setQuestions(prev=>[...prev,{questionText:'',options:['']}])
+}
+function AddOptions(questionIndex:number){
+setQuestions(prev=>{
+    const updated=[...prev]
+    updated[questionIndex].options.push('')
+        return updated
+})
 
+}
+
+
+    return <div> 
+        {
+            questions.map((q,questionIndex)=>(
+                <div className=""></div>
+            ))
+        }
     </div>
 }
